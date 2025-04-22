514a311ff70c655d3807e1692cdf5c4a38b494bf
ParasSidd <142650765+ParasSidd@users.noreply.github.com>
2025-04-22 17:51:50 +0530

    Second commit

--- backend/db/connectDb.js
+++ backend/db/connectDb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDb = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected: ${conn.connection.host}`)
+        console.log('MongoDB connected.')
     } catch (error) {
         console.log("Error connection to MongoDB: ", error.message);
         process.exit(1);
