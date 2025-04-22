1a888416e658242354de07a7b48dc0c30eb9823a
ParasSidd <142650765+ParasSidd@users.noreply.github.com>
2025-04-22 19:05:47 +0530

    eighth commit

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // const __dirname = path.resolve();
 
-app.use(cors({origin: 'http://localhost:5173', credentials: true}));
+app.use(cors({origin: 'https://authenticator-app-frontend-d37u.onrender.com', credentials: true}));
 
 app.use(express.json());
 app.use(cookieParser());
