f5dbaf339ec95b1fd097abbadc61955cbf22c4e2
ParasSidd <142650765+ParasSidd@users.noreply.github.com>
2025-04-22 21:00:31 +0530

    Last Commit

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // const __dirname = path.resolve();
 
-app.use(cors({origin: 'https://authenticator-app-frontend-d37u.onrender.com', credentials: true}));
+app.use(cors({origin: `process.env.CLIENT_URL`, credentials: true}));
 
 app.use(express.json());
 app.use(cookieParser());
